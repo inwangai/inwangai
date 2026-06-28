7aa4637dd1dbfcaf456ba46c376913f6810d15ca
inwangai <admin@inwangai.com>
2026-06-28 11:24:09 +0530

    Add files via upload

---
+++ hrm/sw.js
@@ -0,0 +1,8 @@
+var CACHE='acurate-hrm-v2';
+self.addEventListener('install',function(e){self.skipWaiting();});
+self.addEventListener('activate',function(e){e.waitUntil(clients.claim());});
+self.addEventListener('fetch',function(e){
+  e.respondWith(fetch(e.request).catch(function(){
+    return caches.match(e.request);
+  }));
+});
